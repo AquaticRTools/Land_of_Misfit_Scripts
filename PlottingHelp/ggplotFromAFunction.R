3f2966d1cc048de8d67d9312bac824245e08c0f1
EmmaVJones <emma.v.jones@icloud.com>
2018-01-08 09:02:43 -0500

    Function that outputs ggplot
    This tutorial demonstrates how to make a function that outputs a plot (using ggplot2). This workflow is especially helpful for reports in Rmarkdown or shiny applications that require multiple plots of a similar format with changeable input variables.
    Use Briery2.csv for example dataset from /data

---
+++ PlottingHelp/ggplotFromAFunction.R
@@ -0,0 +1,19 @@
+# How to make function that outputs ggplot with changeable parameter to plot
+
+dataset <- read.csv('data/Briery2.csv')
+parameter <- 'DO'
+
+gMasterPlot <- function(dataset,parameter){
+  dat <- select(dataset,StationID,date,prettyDate,parameter) %>% # Subset parameter of interest from whole dataset
+    na.omit() %>% # Remove samples without parameter measurements
+    dplyr::rename(parameter2=!!names(.[4])) # sneaky rename so ggplot will play nicely in function with changeable parameter variable
+  
+  # Now make the plot
+  p <- ggplot(dat, aes(x=date, y=parameter2, group=StationID)) + 
+    geom_point(aes(fill=StationID),colour='black',shape=21, size=6)
+  # + more plot stuff
+    
+  return(p)
+}
+
+gMasterPlot(Briery2,'DO')
